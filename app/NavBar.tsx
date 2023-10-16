c08b68e827592cb408d762cfe8255986e5f79784
Krittapas Rungsimontuchat <nineten609@gmail.com>
2023-10-16 18:33:59 +0700

    feat:Activelink && install prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
+import classNames from 'classnames'
 import {AiFillBug} from 'react-icons/ai'
+import { usePathname } from 'next/navigation'
+
 const NavBar = () => {
+  const Current_path  = usePathname()
   const links = [
     { href: '/', label: 'Dashboard' },
-    { href: '/' , label: 'Issue' },
+    { href: '/Issue' , label: 'Issue' },
   ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
@@ -13,12 +18,16 @@ const NavBar = () => {
         </Link>
         <ul className='flex space-x-6 '>
           {links.map(item => 
-            <Link key={item.href} href={item.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>
+            <Link key={item.href} href={item.href} className= {classNames({'text-zinc-500 hover:text-zinc-800 transition-colors' :true,
+            'text-zinc-800' : item.href === Current_path,
+            'text-zinc-500' : item.href !== Current_path,
+            })}>
               {item.label}
             </Link>
           )}
         </ul>
     </nav>
+    // 'text-zinc-500 hover:text-zinc-800 transition-colors'
   )
 }
 
